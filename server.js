0dc18a7220ff9d41cbce4d654dca17de74c0d12b
Def <deferenseo@gmail.com>
2024-03-18 17:18:02 +0300

    hooked up mongo Atlas

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const passModule = require('./db_pass')
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors') // <--- important
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const db_pass = passModule.db_pass
 
 app.use(cors()) // <--- important
 
@@ -15,9 +17,16 @@ app.use('/api/boards', require('./routes/boards'));
 
 
 /* mongoose.set('strictQuery', true) */
-mongoose.connect("mongodb://localhost:27017")
+/* mongoose.connect("mongodb://localhost:27017")
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`App working on port: ${port}`);
+        });
+    }); */
+mongoose.connect(`mongodb+srv://deferenseo:${db_pass}@cluster0.n4v6t7m.mongodb.net/sport-shop?retryWrites=true&w=majority&appName=Cluster0`, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         app.listen(port, () => {
             console.log(`App working on port: ${port}`);
         });
     });
+
